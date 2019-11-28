906e2cff1da7fa8f9ab9cc26e85552cad44b0a50
Eugene Malin(Novikov) <jenia96@live.com>
2019-11-28 16:51:01 +0300

    Correct relations

--- lib/relations.ts
+++ lib/relations.ts
@@ -3,7 +3,6 @@ import Message from './model/message'
 import Group from './model/group'
 import Member from './model/member'
 
-Group.hasMany(Message);
 User.hasMany(Message);
 Message.belongsTo(User, {
     foreignKey: 'userId'
