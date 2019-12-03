ff2b5ac8e374cb3f3402f4e1bc64afc2e8f5dee5
Eugene Malin(Novikov) <jenia96@live.com>
2019-12-03 12:30:38 +0300

    Correct task submition

--- app.ts
+++ app.ts
@@ -6,11 +6,12 @@ import logger from './lib/log';
 
 import {connection, IMobileSockets} from './lib/sequelize'
 import './lib/relations';
-import { User } from './lib/relations';
+import { User, Answer } from './lib/relations';
 
 import addUserListeners from './lib/listeners/user'
 import addMessageListeners from './lib/listeners/message'
 import addBotListeners from './lib/listeners/bot'
+import { get } from './lib/engine';
 
 const io: Server = socket(http.createServer().listen(config.get('socketPort')));
 
@@ -33,23 +34,70 @@ connection.sync().then(() => {
 
             addBotListeners(socket, BOT, mobileSockets);
 
-            socket.on('updateGame', (team) => {
-                const tasks: any[] = config.get('game');
-                socket.emit('gameUpdated', {
-                    game: config.get('game'), 
-                    games: [config.get('game')],
-                    task: tasks[teamStates[team.id]] || null
+            const incomingTask = (res: string, socketId: string) => {
+                const data = JSON.parse(res);
+                const parsed: string[] = data['text'].split('\n').map((item: string) => item ? item.trim() : '')
+                const cluesCount = parseInt(((parsed.shift()||'').split("(has")[1]||'0').trim())
+                const question = parsed.shift();
+                const min = Math.round(parseInt(data["span"]) / 60) || 0
+                const sec = Math.round(parseInt(data["span"]) % 60) || 0
+                const explanation = ((parsed.shift()||'').split(':')[1]||'').trim()
+                const clues = parsed.length ? parsed.filter((item: string, index: number) => index % 2) : ['No clues']
+                
+                socket.emit('incomingTask', {
+                    question,
+                    clues,
+                    cluesCount,
+                    delay: {
+                        min,
+                        sec
+                    },
+                    explanation
+                })
+                socket.to(socketId).emit('incomingTask', {
+                    question,
+                    clues,
+                    cluesCount,
+                    delay: {
+                        min,
+                        sec
+                    },
+                    explanation
+                })
+            }
+
+            socket.on('submitTask', ({user, answer}) => {
+                get({}, `/api/gameTracking/checkAnswer/${user.teamToken}@${answer}@${user.gameId}`).then(res => {
+                    const data = JSON.parse(res);
+                    if (!data.isSuccess) {
+                        //TODO добавить статитсику ответов
+                        return socket.emit('showMessage', {
+                            message: `${answer} is not right.`,
+                            type: 'warning',
+                            kind: 'answer',
+                            code: 0
+                        });
+                    }
+                    Object.keys(mobileSockets).forEach(userId => {
+                        if (mobileSockets[userId].teamToken === user.teamToken) {
+                            get({}, `/api/gameTracking/nextStep/${user.teamToken}@${user.gameId}`).then((res: string) => {
+                                incomingTask(res, mobileSockets[userId].socket)
+                            })
+                        }
+                    })
+                    
                 })
             })
-
-            socket.on('enterGame', ({user, game}) => {
-
-            })
-            socket.on('createGame', ({user, game}) => {
-
-            })
-            socket.on('createGame', ({user, game}) => {
-
+            socket.on('updateTask', ({user}) => {
+                get({}, `/api/gameTracking/nextStep/${user.teamToken}@${user.gameId}`).then((res: string) => {
+                    Object.keys(mobileSockets).forEach(userId => {
+                        if (mobileSockets[userId].teamToken === user.teamToken) {
+                            get({}, `/api/gameTracking/nextStep/${user.teamToken}@${user.gameId}`).then((res: string) => {
+                                incomingTask(res, mobileSockets[userId].socket)
+                            })
+                        }
+                    })
+                })
             })
         })    
     })
--- lib/engine.ts
+++ lib/engine.ts
@@ -25,3 +25,27 @@ export function post(data: ParsedUrlQuery, path: string) {
         req.end()
     })
 }
+
+export function get(data: ParsedUrlQuery, path: string) {
+    return new Promise<string>((resolve) => {
+        const req = request({
+            hostname: config.get('engine:host'),
+            port: config.get('engine:port'),
+            path,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+          }, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (body) => {
+                resolve(body);
+            });
+        })
+        req.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+        req.write(JSON.stringify(data))
+        req.end()
+    })
+}
